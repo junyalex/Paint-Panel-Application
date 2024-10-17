7bec2d147e8154d2075261204ff6dfe33a7a2075
leehoju7 <hoju.lee@mail.utoronto.ca>
2024-10-17 11:51:42 +0900

    commited scribble issue in PaintPanel, ScribblePanel

--- src/main/java/ca/utoronto/utm/assignment2/scribble/ScribblePanel.java
+++ src/main/java/ca/utoronto/utm/assignment2/scribble/ScribblePanel.java
@@ -6,16 +6,30 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class ScribblePanel extends Canvas implements EventHandler<MouseEvent>{
+    double startx ,starty;
+
     public ScribblePanel(){
         super(200,200);
-        this.addEventHandler(MouseEvent.MOUSE_MOVED, this);
+        // when we draw, click, move and then
+        this.addEventHandler(MouseEvent.MOUSE_PRESSED, this);
+        this.addEventHandler(MouseEvent.MOUSE_DRAGGED, this);
     }
     @Override
     public void handle(MouseEvent mouseEvent) {
         GraphicsContext gc = this.getGraphicsContext2D();
-        double x = mouseEvent.getX();
-        double y = mouseEvent.getY();
-        gc.setFill(Color.RED);
-        gc.fillRect(x,y,1,1);
+        gc.setFill(Color.BLACK);
+
+        if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED){
+            startx = mouseEvent.getX();
+            starty = mouseEvent.getY();
+        }
+        else if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED){
+            double curx = mouseEvent.getX();
+            double cury = mouseEvent.getY();
+
+            gc.strokeLine(startx, starty, curx, cury);
+            startx = curx;
+            starty = cury;
+        }
     }
 }
