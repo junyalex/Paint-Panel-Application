c2deca3a9a10cbb89e0b6abc00f8286c4e73d79a
frigeretta <123264113+frigeretta@users.noreply.github.com>
2024-10-16 22:46:20 -0400

    Completed fixing BUG1.002/IndicatorofShape

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
@@ -4,14 +4,18 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
         private View view;
+        private Button highlightedButton = null;
+        private ArrayList<Button> buttons;
 
         public ShapeChooserPanel(View view) {
 
                 this.view = view;
-
+                buttons = new ArrayList<>();
                 String[] buttonLabels = { "◯", "▭", "□", "Squiggle (〜)", "Polyline (└───┐)"};
 
                 int row = 0;
@@ -19,14 +23,25 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
                         Button button = new Button(label);
                         button.setMinWidth(100);
                         this.add(button, 0, row);
+                        buttons.add(button);
                         row++;
                         button.setOnAction(this);
                 }
         }
 
+
         @Override
         public void handle(ActionEvent event) {
-                String command = ((Button) event.getSource()).getText();
+                Button clickedButton = (Button) event.getSource();
+                String command = clickedButton.getText();
+
+                for(Button button: buttons){
+                        button.setStyle("");
+                }
+
+                clickedButton.setStyle("-fx-background-color: lightgreen");
+                highlightedButton = clickedButton;
+
                 view.setMode(command);
                 System.out.println(command);
         }
