8af93de74d3227ddbd62ab1cf9647baf5dd5f104
Hojun Lee <hoju.lee@mail.utoronto.ca>
2024-10-24 04:50:39 +0900

    Scribbles complete

--- src/main/java/ca/utoronto/utm/assignment2/paint/ScribbleDrawStrategy.java
+++ src/main/java/ca/utoronto/utm/assignment2/paint/ScribbleDrawStrategy.java
@@ -2,26 +2,55 @@ package ca.utoronto.utm.assignment2.paint;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
 
 public class ScribbleDrawStrategy implements DrawStrategy {
+
+    public Scribble scribble;
+
     public ScribbleDrawStrategy() {
         super();
     }
 
     @Override
     public void onMousePressed(MouseEvent e, PaintModel model) {
-
+        Point startPoint = new Point(e.getX(), e.getY());
+        ArrayList<Point> point_list = new ArrayList<>();
+        point_list.add(startPoint);
+        this.scribble = new Scribble(point_list);
     }
 
     @Override
     public void onMouseDragged(MouseEvent e, PaintModel model) {
+        ArrayList<Point> points = this.scribble.points;
+        double curx = e.getX();
+        double cury = e.getY();
 
+        points.add(new Point(curx, cury));
+        model.addShape(this.scribble);
     }
 
     @Override
     public void onMouseReleased(MouseEvent e, PaintModel model) {
+        double currx = e.getX();
+        double curry = e.getY();
 
+        this.scribble.points.add(new Point(currx, curry));
+        model.addScribble(this.scribble);
+        this.scribble = null;
     }
     @Override
-    public void draw(Shape shape, GraphicsContext g2d) {}
-}
+    public void draw(Shape shape, GraphicsContext g2d) {
+        Scribble scribble = (Scribble) shape;
+        g2d.setFill(Color.GREEN);
+        ArrayList<Point> points = scribble.getPoints();
+            for (int i = 0; i < points.size() - 1 ; i++){
+                Point p1 = points.get(i);
+                Point p2 = points.get(i+1);
+                g2d.strokeLine(p1.x, p1.y, p2.x, p2.y);
+            }
+        }
+    }
+
