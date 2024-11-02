1386646950036c4cb6a367cf5edac6cb14040a54
Hojun Lee <hoju.lee@mail.utoronto.ca>
2024-11-02 13:13:13 +0900

    edit poly

--- src/main/java/ca/utoronto/utm/assignment2/paint/Polyline.java
+++ src/main/java/ca/utoronto/utm/assignment2/paint/Polyline.java
@@ -1,24 +1,29 @@
 package ca.utoronto.utm.assignment2.paint;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Polyline extends Shape{
 
-    private List<Point> points;
+    private ArrayList<Point> points;
+    public static ArrayList<Point> last_points = new ArrayList<>();
+    public static Boolean newPoly = true;
 
-    public Polyline(List<Point> points){
-        this.points = points;
+    public Polyline(){
+        this.points = new ArrayList<>();
     }
 
-    public List<Point> getPoints(){
-        return points;
+    public ArrayList<Point> getPoints(){
+        return this.points;
     }
 
     public void addPoint(Point point){
-        points.add(point);
+        this.points.add(point);
     }
 
     @Override
     public DrawStrategy getDrawStrategy() {
         return new PolylineDrawStrategy();
     }
+
+
 }
--- src/main/java/ca/utoronto/utm/assignment2/paint/PolylineDrawStrategy.java
+++ src/main/java/ca/utoronto/utm/assignment2/paint/PolylineDrawStrategy.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 public class PolylineDrawStrategy implements DrawStrategy {
     private Polyline polyline;
-    private List<Point>points;
+
 
     public PolylineDrawStrategy(){
         super();
-        points = new ArrayList<>();
     }
 
     @Override
     public void onMousePressed(MouseEvent e, PaintModel model) {
         System.out.println("Started Polyline");
-        points.add(new Point(e.getX(), e.getY()));
-        this.polyline = new Polyline(points);
+        if(Polyline.newPoly) Polyline.last_points.add(null);
+        Polyline.newPoly = false;
+
+        this.polyline = new Polyline();
+        this.polyline.getPoints().add(new Point(e.getX(), e.getY()));
+        if (!Polyline.last_points.isEmpty()) {
+            this.polyline.getPoints().add(Polyline.last_points.get(Polyline.last_points.size() - 1));
+        }
         this.polyline.setColor(PaintPanel.color);
     }
 
     @Override
     public void onMouseDragged(MouseEvent e, PaintModel model) {
         if (this.polyline != null) {
-            points.add(new Point(e.getX(), e.getY()));
+            this.polyline.getPoints().add(new Point(e.getX(), e.getY()));
             model.addShapePreview(this.polyline);
         }
     }
@@ -34,7 +39,8 @@ public class PolylineDrawStrategy implements DrawStrategy {
     public void onMouseReleased(MouseEvent e, PaintModel model) {
         if (this.polyline != null) {
             System.out.println("Created Polyline");
-            points.add(new Point(e.getX(), e.getY()));
+            this.polyline.getPoints().add(new Point(e.getX(), e.getY()));
+            Polyline.last_points.add(new Point(e.getX(), e.getY()));
             model.executeCommand(new DrawPolylineCommand(model, polyline));
             this.polyline = null;
         }
@@ -43,20 +49,17 @@ public class PolylineDrawStrategy implements DrawStrategy {
     @Override
     public void draw(Shape shape, GraphicsContext g2d, String currStyle){
         Polyline p = (Polyline) shape;
-        List<Point> points = p.getPoints();
+        ArrayList<Point> points = p.getPoints();
+        g2d.setStroke(p.getColor());
 
-        for(int i =0; i < points.size() - 1; i++){
+        for(int i = 0; i < points.size() - 1; i++) {
             Point p1 = points.get(i);
+            if(p1 == null) continue;
             Point p2 = points.get(i + 1);
-
-            if(currStyle.equals("Outlined")){
-                g2d.setStroke(p.getColor());
-                g2d.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
-            }
-            else if(currStyle.equals("Filled")) {
-                g2d.setFill(p.getColor());
-                g2d.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
-            }
+            if(p2 == null) continue;
+            g2d.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
         }
+
+
     }
 }
