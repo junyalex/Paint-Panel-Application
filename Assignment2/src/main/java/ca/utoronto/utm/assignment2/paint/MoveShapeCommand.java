bbda687657ccb9799800d638f4a84c1f56d5055e
leehoju7 <hoju.lee@mail.utoronto.ca>
2024-11-07 14:33:09 -0500

    edit 4.004

---
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/MoveShapeCommand.java
@@ -0,0 +1,24 @@
+package ca.utoronto.utm.assignment2.paint;
+
+import javafx.scene.input.MouseEvent;
+
+public class MoveShapeCommand implements Command{
+
+    private Shape shape;
+    private PaintModel model;
+
+
+    public MoveShapeCommand(PaintModel model, Shape shape){
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
