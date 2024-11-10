790337901680cf736c2b4024beaa3855ac6dacaa
leehoju7 <hoju.lee@mail.utoronto.ca>
2024-11-10 18:48:58 -0500

    edit

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/CutCommand.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/CutCommand.java
@@ -1,9 +1,24 @@
 package ca.utoronto.utm.assignment2.paint;
 
 public class CutCommand implements Command{
+
+    private PaintModel model;
+    private Shape shape;
+    private Shape selectedShape;
+
+    public CutCommand(PaintModel model, Shape shape) {
+        this.model = model;
+        this.shape = shape;
+        this.selectedShape = this.model.getSelectedShape();
+    }
+
     @Override
     public void execute() {
-
+        for (Shape s : model.getShapes()){
+            if(s.equals(selectedShape)){
+                
+            }
+        }
     }
 
     @Override
