98e793342fc90bfa952f04bed9ffa65622da997d
leehoju7 <hoju.lee@mail.utoronto.ca>
2024-11-10 18:38:13 -0500

    edit command

---
+++ src/main/java/ca/utoronto/utm/assignment2/paint/CutCommand.java
@@ -0,0 +1,13 @@
+package ca.utoronto.utm.assignment2.paint;
+
+public class CutCommand implements Commandq{
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
